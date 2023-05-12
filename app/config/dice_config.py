cd1c7f8ace49a195cd3fda9466c2a60697481423
Leo Chung <leochungus@berkeley.edu>
2023-05-12 11:05:43 -0700

    fixed die naming bug

--- app/config/dice_config.py
+++ app/config/dice_config.py
@@ -10,9 +10,9 @@ dice_config = {
     "heal1": [[1, 2, 3, 4, 5, 6], 1, 600, "heal", True],
     "heal2": [[1, 2, 3, 4, 5, 6], 2, 600, "heal", True],
     "heal3": [[1, 2, 3, 4, 5, 6], 3, 600, "heal", True],
-    "divider1": [[1, 1, 1, 1, 2, 2], 1, 1000, "divide", True],
-    "divider2": [[1, 1, 1, 2, 2, 2], 1, 1000, "divide", True],
-    "divider3": [[1, 1, 2, 2, 2, 3], 1, 1000, "divide", True],
+    "divider1": [[1, 1, 1, 1, 2, 2], 1, 1000, "divider", True],
+    "divider2": [[1, 1, 1, 2, 2, 2], 1, 1000, "divider", True],
+    "divider3": [[1, 1, 2, 2, 2, 3], 1, 1000, "divider", True],
     "multiplier1": [[1, 1, 1, 1, 2, 2], 1, 3000, "multiplier", True],
     "multiplier2": [[1, 1, 1, 2, 2, 2], 1, 3000, "multiplier", True],
     "multiplier3": [[1, 1, 2, 2, 2, 3], 1, 3000, "multiplier", True]
